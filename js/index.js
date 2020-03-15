c7592d1341ee47b67971f72e97d9d7cc962d0ff3
Neil Menon <steelneil12@gmail.com>
2020-03-15 18:13:21 -0400

    Developed & implemented a Last.fm integration to display my most recently played song

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    $.get('/lastfm.php', function(data) {
+        var track_html = [];
+        var tracks = JSON.parse(data);
+        console.log(tracks)
+        tracks.forEach(function(track, index) {
+            console.log(index + ". " + track.name + " by " + track.artist['#text'] + " on " + track.album['#text'] + " // " + played)
+            var title = track.name
+            var artist = track.artist['#text']
+            var album = track.album['#text']
+            var url = track.url
+            var image = track.image[1]['#text']
+            var played = "";
+            var scrobble = "";
+            if (typeof track.date !== "undefined") {
+                var played = "Played " + moment.unix(track.date.uts).fromNow()
+            } else {
+                var played = "Listening now on Spotify"
+                scrobble = "listening"
+            }
+            track_html.push('<span style="float:left"> <a href="' + url + '" target="_blank"> <img title="View this track on Last.fm" src="' + image + '"> </a> </span> <div class="lastfm-details"> <span class="' + scrobble + '" title="Track: ' + title + '"><strong>' + title + '</strong></span> <br> <span title="Artist: ' + artist + '"> <i class="fa fa-user" aria-hidden="true"></i> ' + artist + '</span> <br> <span class="album" title="Album: ' + album + '">' + album + '</span><br> </div> <span class="lastfm-played"><hr>' + played + '</span>')
+        });
+        document.getElementById("lastfm").innerHTML = track_html[0];
+    });
+})
 let vh = window.innerHeight * 0.01;
 document.body.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
@@ -5,66 +30,67 @@ window.addEventListener('resize', () => {
     document.body.style.setProperty('--vh', `${vh}px`);
 });
 
-const address = "nkm5gz", domain = "virginia.edu"
+const address = "nkm5gz",
+    domain = "virginia.edu"
 $("#mail a").attr("href", "mailto:" + address + "@" + domain)
 
 $("#mail a").hover(function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeOut(0)
+    $("#socialsText").fadeTo(0, 0.001)
     $("#socialsText").text("Send me an email")
-    $("#socialsText").fadeIn(500)
-}, function () {
+    $("#socialsText").fadeTo(500, 1)
+}, function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeIn(0)
-    $("#socialsText").fadeOut(500)
+    $("#socialsText").fadeTo(0, 1)
+    $("#socialsText").fadeTo(500, 0.001)
 });
 $("#github a").hover(function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeOut(0)
+    $("#socialsText").fadeTo(0, 0.001)
     $("#socialsText").text("Check out my code on Github")
-    $("#socialsText").fadeIn(500)
-}, function () {
+    $("#socialsText").fadeTo(500, 1)
+}, function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeIn(0)
-    $("#socialsText").fadeOut(500);
+    $("#socialsText").fadeTo(0, 1)
+    $("#socialsText").fadeTo(500, 0.001);
 });
 $("#resume a").hover(function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeOut(0)
+    $("#socialsText").fadeTo(0, 0.001)
     $("#socialsText").text("Check out my résumé")
-    $("#socialsText").fadeIn(500)
-}, function () {
+    $("#socialsText").fadeTo(500, 1)
+}, function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeIn(0)
-    $("#socialsText").fadeOut(500)
+    $("#socialsText").fadeTo(0, 1)
+    $("#socialsText").fadeTo(500, 0.001)
 });
 $("#linkedin a").hover(function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeOut(0)
+    $("#socialsText").fadeTo(0, 0.001)
     $("#socialsText").text("Add me on Linkedin")
-    $("#socialsText").fadeIn(500)
-}, function () {
+    $("#socialsText").fadeTo(500, 1)
+}, function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeIn(0)
-    $("#socialsText").fadeOut(500)
+    $("#socialsText").fadeTo(0, 1)
+    $("#socialsText").fadeTo(500, 0.001)
 });
 $("#instagram a").hover(function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeOut(0)
+    $("#socialsText").fadeTo(0, 0.001)
     $("#socialsText").text("Follow me on Instagram")
-    $("#socialsText").fadeIn(500)
-}, function () {
+    $("#socialsText").fadeTo(500, 1)
+}, function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeIn(0)
-    $("#socialsText").fadeOut(500)
+    $("#socialsText").fadeTo(0, 1)
+    $("#socialsText").fadeTo(500, 0.001)
 });
 $("#spotify a").hover(function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeOut(0)
+    $("#socialsText").fadeTo(0, 0.001)
     $("#socialsText").text("Follow me on Spotify")
-    $("#socialsText").fadeIn(500)
-}, function () {
+    $("#socialsText").fadeTo(500, 1)
+}, function() {
     $("#socialsText").stop()
-    $("#socialsText").fadeIn(0)
-    $("#socialsText").fadeOut(500)
+    $("#socialsText").fadeTo(0, 1)
+    $("#socialsText").fadeTo(500, 0.001)
 });
