c38d5835fa18f1b871209e20f141d4ccf07689d2
neilmenon <steelneil12@gmail.com>
2021-12-24 02:40:54 -0500

    Bio update + IMDB feature

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', () => {
     updateListeningActivity();
+    getRecentlyWatched();
     window.setInterval(function(){
         if (document.visibilityState == "visible") { // if the DOM is visible (currently on tab)
             updateListeningActivity();
@@ -31,15 +32,69 @@ function updateListeningActivity() {
     })
 }
 
+function getRecentlyWatched() {
+    $.get(`/imdb.json?t=${Date.now()}`, function(payload) {
+        let data = JSON.parse(JSON.stringify(payload))
+        let movie = data?.latest
+        if (movie) {
+            $("#movie").html(`<span style="float: left;"> <a href="${movie.link}" target="_blank" title="${movie.description}"><img src="${movie.image_url}" style="max-height: 96px;"></a> </span> <div style="float: right;" class="movie-details"> <span style="font-weight: 900;"></span>${movie.title}<br> <span>${movie.year} • ${movie.rating} • ${movie.genres.join(", ")}</span><br> <span title="Director"><i class="fa fa-user" aria-hidden="true"></i> ${movie.director} • <span title="Votes: ${shortNumber(movie.votes.replaceAll(",", ""))}">IMDB: ${movie.imdb_rating}</span></span><br> <span title="Rated/watched on ${moment(movie.rated_on).format("MMMM Do, YYYY")}"><u>My Rating</u>: ${movie.my_rating} — "${getRatingDescriptor(movie.my_rating)}" • <i class="fas fa-clock"></i> ${moment(movie.rated_on).format("MMM Do")}</span><br></div>`)
+        } else {
+            $("#movie").html()
+        }
+    })
+}
+
+function shortNumber(number) {
+    if (isNaN(number)) return null; // will only work value is a number
+    if (number === null) return null;
+    if (number === 0) return null;
+    let abs = Math.abs(number);
+    const rounder = Math.pow(10, 1);
+    const isNegative = number < 0; // will also work for Negetive numbers
+    let key = '';
+
+    const powers = [
+        {key: 'Q', value: Math.pow(10, 15)},
+        {key: 'T', value: Math.pow(10, 12)},
+        {key: 'B', value: Math.pow(10, 9)},
+        {key: 'M', value: Math.pow(10, 6)},
+        {key: 'K', value: 1000}
+    ];
+
+    for (let i = 0; i < powers.length; i++) {
+        let reduced = abs / powers[i].value;
+        reduced = Math.round(reduced * rounder) / rounder;
+        if (reduced >= 1) {
+            abs = reduced;
+            key = powers[i].key;
+            break;
+        }
+    }
+    return (isNegative ? '-' : '') + abs + key;
+}
+
+function getRatingDescriptor(rating) {
+    let ratingDescriptors = {
+        "1": "Worst",
+        "2": "???",
+        "3": "Unacceptable",
+        "4": "Bad",
+        "5": "Mediocre",
+        "6": "Passable",
+        "7": "Good",
+        "8": "Solid",
+        "9": "Almost Perfect",
+        "10": "Transcendent",
+    }
+    return ratingDescriptors[rating]
+}
+
 let vh = window.innerHeight * 0.01
 document.body.style.setProperty('--vh', `${vh}px`)
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01
     document.body.style.setProperty('--vh', `${vh}px`)
 })
-
-const address = "nkm5gz", domain = "virginia.edu"
-$("#mail a").attr("href", "mailto:" + address + "@" + domain)
 
 $("#mail a").hover(function() {
     $("#socialsText").stop()
