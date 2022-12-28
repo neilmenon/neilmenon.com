44d1dacc67663861e15bf673d82a82d6f0b4e671
neilmenon <steelneil12@gmail.com>
2022-12-28 11:48:29 -0500

    Performance optimizations and minor tweaks

--- js/index.js
+++ js/index.js
@@ -11,25 +11,51 @@ window.addEventListener('DOMContentLoaded', () => {
 
 function updateListeningActivity() {
     $.get('/lastfm.php', function(data) {
-        var track_html = []
-        var tracks = JSON.parse(data)
-        tracks.forEach(function(track, index) {
-            var title = track.name
-            var artist = track.artist['#text']
-            var album = track.album['#text']
-            var url = track.url
-            var image = track.image[1]['#text']
+        var track_html = ""
+        var track = data?.tracks[0]
+        if (!track) { return }
+        
+        var title = track.name
+        var artist = track.artist['#text']
+        var album = track.album['#text']
+        var url = track.url
+        var image = track.image[2]['#text']
+        var played = ""
+        var scrobble = ""
+        if (typeof track.date !== "undefined") {
+            moment.locale('en-short', {
+                relativeTime: {
+                    future: 'in %s',
+                    past: '%s',
+                    s:  '%ds',
+                    ss: '%ds',
+                    m:  '1m',
+                    mm: '%dm',
+                    h:  '1h',
+                    hh: '%dh',
+                    d:  '1d',
+                    dd: '%dd',
+                    M:  '1mo',
+                    MM: '%dmo',
+                    y:  '1y',
+                    yy: '%dY'
+                }
+                });
+            var played = '<i class="fas fa-clock"></i> ' + moment.unix(track.date.uts).locale('en-short').fromNow() + ' • '
+        } else {
             var played = ""
-            var scrobble = ""
-            if (typeof track.date !== "undefined") {
-                var played = "Played " + moment.unix(track.date.uts).fromNow()
-            } else {
-                var played = "Listening now"
-                scrobble = "listening"
-            }
-            track_html.push('<div style="height:77px"><span style="float:left"> <a href="' + url + '" target="_blank"> <img title="View this track on Last.fm" src="' + image + '"> </a> </span> <div class="lastfm-details"> <span class="' + scrobble + '" title="Track: ' + title + '"><strong>' + title + '</strong></span> <br> <span title="Artist: ' + artist + '"> <i class="fa fa-user" aria-hidden="true"></i> ' + artist + '</span> <br> <span class="album" title="Album: ' + album + '">' + album + '</span><br> </div></div> <div class="lastfm-played"><hr>' + played + '</div>')
-        })
-        document.getElementById("lastfm").innerHTML = track_html[0]
+            scrobble = "listening"
+        }
+
+        var blacklistedGenres = [artist?.toLowerCase(), 'seen live', 'female vocalists', 'american', 'male vocalists', 'usa', 'all', 'under 2000 listeners', 'vocal', 'indie', 'pop', 'electronic', 'rock', 'hip hop', 'rap', 'folk', 'alternative', 'singer songwriter', 'british', 'emo']
+        var genreFilterBlacklist = data?.genre?.toptags?.tag?.filter(x => !blacklistedGenres.includes(x?.name?.toLowerCase()))
+        
+        var genericGenres = ['indie', 'pop', 'electronic', 'rock', 'hip hop', 'rap', 'folk', 'alternative', 'singer songwriter']
+        var genreFilterGeneric = data?.genre?.toptags?.tag?.filter(x => genericGenres.includes(x?.name?.toLowerCase()))
+        var genre = genreFilterBlacklist.length ? genreFilterBlacklist[0].name : (genreFilterGeneric.length ? genreFilterGeneric[0].name : 'N/A')
+
+        track_html = '<div><span style="float:left"> <a href="' + url + '" target="_blank" title="View this track on Last.fm"> <img title="Album Artwork" src="' + image + '" width="96px" height="96px"> </a> </span> <div class="movie-details" style="line-height: 21px; word-break: break-word; display: inline-block; max-width: 230px;"> <span class="' + scrobble + '" title="Track: ' + title + '"><strong>' + title + '</strong></span> <br> <span title="Artist: ' + artist + '"> <i class="fa fa-user" aria-hidden="true"></i> ' + artist + '</span> <br> <span title="Album: ' + album + '"><i class="fas fa-record-vinyl"></i> ' + album + '</span><br> ' + played + '<span title="Genre"><i class="fas fa-music"></i> ' + genre?.replace(/\b\w/g, l => l.toUpperCase()) + '</span></div></div>'
+        document.getElementById("lastfm").innerHTML = track_html
     })
 }
 
@@ -43,7 +69,7 @@ function getRecentlyWatched() {
         }
         let ratingString = ratingStrings.join(", ")
         if (movie) {
-            $("#movie").html(`<span style="float: left;"> <a href="${movie.link}" target="_blank" title="${movie.description}"><img src="${movie.image_url}" style="max-height: 96px;"></a> </span> <div style="float: right;" class="movie-details"> <span style="font-weight: 900;" title="${movie.title}">${movie.title}</span><br> <span>${movie.year} • ${movie.rating} • ${movie.genres.join(", ")}</span><br> <span title="Director"><i class="fa fa-user" aria-hidden="true"></i> ${movie.director} • <span title="Votes: ${shortNumber(movie.votes.replaceAll(",", ""))}">IMDB: ${movie.imdb_rating}</span></span><br> <span title="${ratingString}"><u>My Rating</u>: ${movie.my_rating} — "${getRatingDescriptor(movie.my_rating)}" • <i class="fas fa-clock"></i> ${moment(movie.rated_on).format("MMM Do")}</span><br></div>`)
+            $("#movie").html(`<span style="float: left;"> <a href="${movie.link}" target="_blank" title="${movie.description}"><img title="${movie.title} Poster" src="${movie.image_url}" width="64px" height="96px"></a> </span> <div style="float: right;" class="movie-details"> <span style="font-weight: 900;" title="${movie.title}">${movie.title}</span><br> <span>${movie.year} • ${movie.rating} • ${movie.genres.join(", ")}</span><br> <span title="Director"><i class="fa fa-user" aria-hidden="true"></i> ${movie.director} • <span title="Votes: ${shortNumber(movie.votes.replaceAll(",", ""))}">IMDB: ${movie.imdb_rating}</span></span><br> <span title="${ratingString}"><u>My Rating</u>: ${movie.my_rating} — "${getRatingDescriptor(movie.my_rating)}" • <i class="fas fa-clock"></i> ${moment(movie.rated_on).format("MMM Do")}</span><br></div>`)
         } else {
             $("#movie").html()
         }
@@ -54,8 +80,13 @@ function getLatestConcert() {
     $.get(`/songkick.json?t=${Date.now()}`, function(payload) {
         let data = JSON.parse(JSON.stringify(payload))
         let concert = data
+        let isHeadSupportOneLine = concert.headliner_artist.includes(", and ")
+        if (isHeadSupportOneLine) {
+            concert.supporting_artist = concert.headliner_artist.replace(concert.headliner_artist.split(",")[0] + ", ", "").replace(", and ", ", ")
+            concert.headliner_artist = concert.headliner_artist.split(",")[0]
+        }
         if (concert) {
-            $("#concert").html(`<span style="float: left;"> <a href="${concert.sk_link}" target="_blank" title="View on Songkick"><img src="${concert.artist_image}" style="max-height: 96px;"></a> </span> <div style="float: right;line-height: 21px; max-width: 68%" class="movie-details"> <span style="font-weight: 900;" title="${concert.headliner_artist}">${concert.headliner_artist}</span><br> <span>${concert.supporting_artist ? "w/ " + concert.supporting_artist : "<em>No Supporting Acts</em>"}</span><br> <span title="Venue"><i class="fas fa-map-marker-alt"></i> ${concert.venue}</span><br> <span title="${concert.timestamp}"><i class="fas fa-clock"></i> ${moment(concert.timestamp).format("dddd, MMMM Do, YYYY")}</span><br></div>`)
+            $("#concert").html(`<span style="float: left;"> <a href="${concert.sk_link}" target="_blank" title="View on Songkick"><img title="${concert.headliner_artist} Image" src="${concert.artist_image}" width="96px" height="96px"></a> </span> <div style="float: right;line-height: 21px; max-width: 230px" class="movie-details"> <span style="font-weight: 900;" title="${concert.headliner_artist}">${concert.headliner_artist}</span><br> <span title="Supporting Artist">${concert.supporting_artist ? "w/ " + concert.supporting_artist : "N/A"}</span><br> <span title="Venue"><i class="fas fa-map-marker-alt"></i> ${concert.venue}</span><br> <span title="${concert.timestamp}"><i class="fas fa-clock"></i> ${moment(concert.timestamp).format("dddd, MMMM Do, YYYY")}</span><br></div>`)
         } else {
             $("#concert").html()
         }
